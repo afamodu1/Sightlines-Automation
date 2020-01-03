a8e8ccda3351a6aef3485d32dfba550f8af8934b
afamodu1 <58476735+afamodu1@users.noreply.github.com>
2020-01-03 14:36:47 -0500

    Update Geocoder.R

--- Geocoder.R
+++ Geocoder.R
@@ -15,11 +15,11 @@ library(leaflet) # Will use to demo maps that can be created
 library(tidyverse)
 
 {
-key = 
+key =  # Place Google API Key Here
 
 register_google(key = key)
 
-} #Contains my API Key Please Do Not Take!!!
+} #
 
 holder = c()
 
@@ -37,16 +37,12 @@ ui <- fluidPage(
         p("Once the data is downloaded check the returned addresses to ensure that they are not just the town and state, this is an error and means that the program could not find the address by asking google"),
         textInput("Town", "Choose a Town", value = ""),
         textInput("State", "Choose a State", value = ""),
-        #textInput("Anchor", "Choose a Building I.D"),
-        #selectInput("Mode", "What Type of Transit Should be Used", choices =  c("walking", "driving", "transit"), selected = NULL),
-        #textInput("Radius", "What is the maximum radius you would like to include from this building", value = ""),
          fileInput("Geocode_File", "Choose CSV File", accept = c("text/csv", "text/comma-seperated-values,text/plain",".csv")) # defines a file input as well as the expectations for it
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
         downloadButton("download_data", "Download Addresses"), # creates a button for downloading information from the internet
-        downloadButton("download_na", "Download NA Values"), # Creates a button to download NA values that could not be geocoded
         tableOutput("contents"),
         leafletOutput("Mymap"),
         textOutput("Here")
@@ -73,47 +69,7 @@ server <- function(input, output) {
     df$ADDRESS = gsub( "*\\(.*?\\) *","", df$ADDRESS)
     df$ADDRESS = paste(df$ADDRESS,", ", input$Town, ", ", input$State)
     df = mutate_geocode(df, ADDRESS, output = "latlona")
-     
-      #anchor = input$Anchor # Set a central anchor point to map drive time
-     
-     
 
-      #method = input$Mode # Selection for the mode of travel
-     
-     
-     
-     #radius = as.numeric(input$Radius)
-     
-     
-     #anchor2= df$ADDRESS[df$Building.Number..or.Identifier. == anchor]
-     
-     
-     
-     #test2 = function(address) {
-      # mapdist(anchor2, address, mode = method, output = "simple")
-     #} #Helper code to help the direction converter to code accurately set to accept driving on default
-     
-     #df_2 = df[!is.na(df$address2), ]
-     
-     #data_frame_2 = lapply(df$ADDRESS, test2) # maps driving distance
-     
-     
-     
-     #data_frame_3 = data.frame(matrix(unlist(data_frame_2),nrow= length(data_frame_2), byrow=9)) #create a new dataframe that contains ditance time 
-     
-     #names(data_frame_3) <- c("Building Point", "Anchor", "Meters", "Kilometers", "Miles", "Seconds", "Minutes", "Hours", "Method") #Names the new data frame
-     
-     #cols = c("Meters", "Kilometers", "Miles", "Seconds", "Minutes", "Hours") #Name the columms that need to become number
-     
-     #test_3 = function(num) {
-       #as.numeric(as.character(num))
-     #} # Helper function to turn factor columns into number columns
-     
-     #data_frame_3[cols] = lapply(data_frame_3[cols], test_3) #Turn factor columns into a number columns
-     
-     #df$Minutes = data_frame_3$Minutes #Concatenate drive time in minutes
-     
-     #df$`Include/Exclude` = ifelse(df$Minutes >= radius, "Exclude", "Include") # Create Include, Exclude based on driving minutes
      
      output$Mymap = renderLeaflet(leaflet(data = df) %>% addTiles() %>% addMarkers(~lon, ~lat, label = ~as.character(`Building.Name`))) #Output the map
      
@@ -136,12 +92,6 @@ server <- function(input, output) {
     } # Formats the download button
   )
   
-  output$download_na = downloadHandler(
-    "NA Address Values.csv", content = function(file) {
-      write.csv(newframe(),file)
-    } #writes the files that could not be geocoded into a 
-  )
-}
  
 
 # Run the application 
